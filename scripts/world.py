8d2cf9907ae74bc52e145f2c51df9c77cc647259
Noah Nguyen <nohanguyenn@gmail.com>
2023-12-01 20:36:48 -0500

    optimizing hopefully

--- scripts/world.py
+++ scripts/world.py
@@ -21,6 +21,23 @@ class World:
         self.builder_mode = False
         self.show_builder_menu = False
 
+        """ self.world_rects = {}
+        for i, obstacle in enumerate(config['obst_hitboxes']['tutorial']):
+            self.world_rects[i] = self.set_obstacle_hitbox(i, obstacle) """
+
+    def set_obstacle_hitbox(self, idx, offset):
+        img = self.game.assets.collideables[str(idx)]
+        hitbox = (offset[2] + offset[0] // 2, offset[1], img.get_width() + offset[0], img.get_height() + offset[1])
+        return pygame.Rect(hitbox)
+
+    def obs_rect(self, tile, img, idx):
+        # 0 -> big tree, 1 -> short tree, 2 -> alive bush, 3 -> stump, 4 -> fence, 5 -> log, 6 -> right-facing lamp, 7 -> down-facing lamp, 8 -> left-facing lamp, 9 -> broken lamp
+        offset = config['obst_hitboxes']['tutorial'][idx]
+        coord = (tile[0], tile[1])
+        hitbox = (coord[0] - (offset[0] // 2) + offset[2], coord[1] - offset[1], img.get_rect().width + offset[0], img.get_rect().height + offset[1])
+        #pygame.draw.rect(self.game.window.display, 'red', (hitbox[0] - self.camera.true_pos[0], hitbox[1] - self.camera.true_pos[1], hitbox[2], hitbox[3]), 1)
+        return pygame.Rect(hitbox)
+
     def load(self, map_id):
         self.floor = self.game.assets.maps[map_id]
         self.map_data = import_csv_layout('data/maps/' + map_id + '/' + map_id + '_Collideables.csv')
@@ -70,7 +87,9 @@ class World:
                     x = col_index * 16
                     y = row_index * 16
                     img = self.game.assets.collideables[col]
-                    #self.collideables.append(self.obs_rect((x + offset[0], y + offset[1] - img.get_size()[1]), img, int(col)))
+                    self.collideables.append(self.obs_rect((x + offset[0], y + offset[1] - img.get_size()[1]), img, int(col)))
+                    #rect = self.world_rects[int(col)]
+                    #self.collideables.append((x + offset[0] - rect[0], y + offset[1] - rect[1], rect[2], rect[3]))
                     if col != '10':
                         self.render_list.append([img, (x + offset[0] - self.camera.true_pos[0], y + offset[1] - self.camera.true_pos[1] - img.get_size()[1])])
 
@@ -80,14 +99,6 @@ class World:
         self.destruction_particles.render(surf, self.camera.true_pos)
         self.vfx.render_front(surf, self.camera.true_pos)
         self.quadtree.show(surf, self.camera.true_pos)
-
-    def obs_rect(self, tile, img, idx):
-        # 0 -> big tree, 1 -> short tree, 2 -> alive bush, 3 -> stump, 4 -> fence, 5 -> log, 6 -> right-facing lamp, 7 -> down-facing lamp, 8 -> left-facing lamp, 9 -> broken lamp
-        offset = config['obst_hitboxes']['tutorial'][idx]
-        coord = (tile[0], tile[1])
-        hitbox = (coord[0] - (offset[0] // 2) + offset[2], coord[1] - offset[1], img.get_rect().width + offset[0], img.get_rect().height + offset[1])
-        #pygame.draw.rect(self.game.window.display, 'red', (hitbox[0] - self.camera.true_pos[0], hitbox[1] - self.camera.true_pos[1], hitbox[2], hitbox[3]), 1)
-        return pygame.Rect(hitbox)
         
     def update(self):
         self.camera.update()
